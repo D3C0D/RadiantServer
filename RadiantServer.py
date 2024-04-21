d0ab354ac7a79cd4538531a69d3fc6e81dd360f6
D3C0D <luis.chacon.mora@gmail.com>
2024-04-20 23:28:05 -0600

    Commit from the project V1

---
+++ RadiantServer.py
@@ -0,0 +1,83 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import webbrowser
+import configparser
+import os
+
+# Load configuration file at runtime or create one with default values if it doesn't exist
+config = configparser.ConfigParser()
+if not config.read('web.config'):
+    # generate default values
+    config.add_section('server')
+    config.set('server', 'port', '8000')
+    config.set('server', 'address', '')
+    config.set('server', 'index_file', 'index.html')
+    with open('web.config', 'w') as file:
+        config.write(file)
+
+# Create the web folder if it doesn't exist
+web_folder_path = 'web'
+if not os.path.exists(web_folder_path):
+    os.makedirs(web_folder_path)
+# Create the index.html file into the web folder if it doesn't exist
+index_html_file_path = f'{web_folder_path}/{config.get('server', 'index_file')}'
+if not os.path.exists(index_html_file_path):
+    with open(index_html_file_path, 'w') as f:
+        f.write('''
+                    <html>
+                    <head>
+                    <title>RadiantServer</title>
+                    <style>
+                        body {
+                        background-color: #000;
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        height: 100vh; /* Set the height of the page to match the screen height */
+                        }
+                        h1 {
+                        color: gold;
+                        font-size: 64px;
+                        text-align: center;
+                        }
+                        h2 {
+                        color: green;
+                        font-size: 52px;
+                        text-align: center;
+                        }
+                    </style>
+                    </head>
+                    <body>
+                    <h1>RadiantServer</h1>
+                    <br>
+                    <h2>Is Online</h2>
+                    </body>
+                    </html>
+                ''')
+
+config = configparser.ConfigParser()
+if not config.read('web.config'):
+    # generate default values
+    config.add_section('server')
+    config.set('server', 'port', '8000')
+    config.set('server', 'address', '')
+    config.set('server', 'index_file', 'index.html')
+    with open('web.config', 'w') as file:
+        config.write(file)
+
+class OalamaChatsBaseHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        index_html = open("web/"+config.get('server', 'index_file'), 'r').read()
+        self.wfile.write(index_html.encode('utf-8'))
+
+if __name__ == "__main__":
+    server_address = (config.get('server', 'address'), int(config.get('server', 'port')))
+    httpd = HTTPServer(server_address, OalamaChatsBaseHandler)
+    print("RadiantServer is running...")
+    webbrowser.open_new_tab(f'http://localhost:{httpd.server_port}/')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+         print("Server Stopped")
